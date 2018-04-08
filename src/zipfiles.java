9b376d606a8ecbd5fc88e30b0b45e929816d7aec
Santosh Gulshetty <s.gulshetty>
2018-04-08 11:52:05 +0530

    changes added

--- src/zipfiles.java
+++ src/zipfiles.java
@@ -1,8 +1,42 @@
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class zipfiles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			//start of zip file code
+			FileOutputStream fos = new FileOutputStream("atest.zip");
+			ZipOutputStream zos = new ZipOutputStream(fos);
+
+			String file1Name = "file1.txt";
+			String file2Name = "file2.txt";
+			String file3Name = "folder/file3.txt";
+			String file4Name = "folder/file4.txt";
+			String file5Name = "f1/f2/f3/file5.txt";
+
+			addToZipFile(file1Name, zos);
+			addToZipFile(file2Name, zos);
+			addToZipFile(file3Name, zos);
+			addToZipFile(file4Name, zos);
+			addToZipFile(file5Name, zos);
+
+			zos.close();
+			fos.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	}
 
